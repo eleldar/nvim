9787b94ad516040a040a91632670652ba9249ddf
Eldar Boltachev <eleldar@mail.ru>
2025-01-17 02:42:16 +0300

    fix confirm

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -9,7 +9,6 @@ return {
 			local cmp = require('cmp')
 			cmp.setup({
 				snippet = {
-					-- REQUIRED - you must specify a snippet engine
 					expand = function(args)
 						vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
 						-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
@@ -27,7 +26,7 @@ return {
 					['<C-f>'] = cmp.mapping.scroll_docs(4),
 					['<C-Space>'] = cmp.mapping.complete(),
 					['<C-e>'] = cmp.mapping.abort(),
-					['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					-- ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 					['<Tab>'] = cmp.mapping(function(fallback)
                         if cmp.visible() then
                             cmp.select_next_item()
@@ -49,6 +48,7 @@ return {
 					-- { name = 'luasnip' }, -- For luasnip users.
 					-- { name = 'ultisnips' }, -- For ultisnips users.
 					-- { name = 'snippy' }, -- For snippy users.
+					-- 
 				}, {
 					{ name = 'buffer' },
 				})
