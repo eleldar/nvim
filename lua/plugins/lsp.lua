f4faaafe37632ec403faae098529a62694ba6d85
Eldar Boltachev <eleldar@mail.ru>
2025-01-19 03:28:57 +0300

    comfortable imaging

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,9 +3,8 @@ return {
 		'neovim/nvim-lspconfig',
 		config = function()
 			local lspconfig = require('lspconfig')
-			local capabilities = require('cmp_nvim_lsp').default_capabilities()
 			lspconfig.pyright.setup({
-				capabilities = capabilities
+				capabilities = require('cmp_nvim_lsp').default_capabilities()
 			})
 			lspconfig.lua_ls.setup({})
 			lspconfig.ts_ls.setup({})
